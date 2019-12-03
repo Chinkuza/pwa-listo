a906de09013715607e63f0144831d115cff30cf4
Chinkuza <j.rosarioc2h@gmail.com>
2019-12-03 12:06:43 -0500

    Fixed port issues on package.json and rebuilt structure.

---
+++ client/build/precache-manifest.4b4ce013a031781354b16106de3bc0a0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feadb57f51c386d08eb94e9053915458",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1177261f0e31c4d2f77",
+    "url": "/static/css/2.18585a14.chunk.css"
+  },
+  {
+    "revision": "f51d174ba95acc08d68a",
+    "url": "/static/css/main.177cd642.chunk.css"
+  },
+  {
+    "revision": "f1177261f0e31c4d2f77",
+    "url": "/static/js/2.a2392ac2.chunk.js"
+  },
+  {
+    "revision": "f51d174ba95acc08d68a",
+    "url": "/static/js/main.27edd03a.chunk.js"
+  },
+  {
+    "revision": "ecaa1186f1a396b1c131",
+    "url": "/static/js/runtime-main.e88a423c.js"
+  },
+  {
+    "revision": "2e4a7b76b243c4ceb0fe8d75efc53139",
+    "url": "/static/media/herohome.2e4a7b76.jpg"
+  },
+  {
+    "revision": "e723dc80b3f57b4f14fbfc7d8661804e",
+    "url": "/static/media/homephone5.e723dc80.jpg"
+  },
+  {
+    "revision": "2ef6486169a9ac99013f19e4d880b1ed",
+    "url": "/static/media/homeproduct2.2ef64861.png"
+  },
+  {
+    "revision": "09a63c1af14bed142bb66d893d1441e4",
+    "url": "/static/media/homeproduct3.09a63c1a.jpg"
+  },
+  {
+    "revision": "fec7c735aadb5b07f9000849759ddd0d",
+    "url": "/static/media/homeproduct4.fec7c735.jpg"
+  },
+  {
+    "revision": "4a18d106453a87330e216a1f9cfa9212",
+    "url": "/static/media/homeproducts1.4a18d106.jpg"
+  },
+  {
+    "revision": "b78ef37339cfe8e7c680f15263800c6d",
+    "url": "/static/media/homeproducts6.b78ef373.jpg"
+  },
+  {
+    "revision": "0436d850a2d1162166b19bd3f60e4531",
+    "url": "/static/media/orangephone.0436d850.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
